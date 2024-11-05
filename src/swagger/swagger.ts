19c17c3e0d59b91753613edece9f886ac1a48ef9
DavidCTQ <hidenrd@outlook.es>
2024-11-05 07:10:04 -0500

    Agrego swagger

---
+++ src/swagger/swagger.ts
@@ -0,0 +1,23 @@
+import { url } from "inspector";
+import swaggerJSDoc, { Options } from "swagger-jsdoc";
+const swaggerOptions: Options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Backen Service API",
+      version: "Beta",
+      description: "API para catálogo de productos.",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000/",
+      },
+    ],
+  },
+  apis: [
+    "./src/routes/productRoutes.ts",
+  ],
+};
+
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
+export default swaggerSpec;
